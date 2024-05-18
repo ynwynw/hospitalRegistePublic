4582b80afe19891a4e787461e912c4a2904c48b1
ynw <931708230@qq.com>
2024-05-18 11:56:10 +0800

    first commit

--- hospital-registration-wx/api/api.js
+++
@@ -1,9 +0,0 @@
-import request from "@/utils/request.js";
-/*
-管理员账户登录
-*/
-export function login(data) {
-	return request.post("/login", data, {
-		noAuth: true,
-	});
-}
--- hospital-registration-wx/store/getters.js
+++
@@ -1,6 +0,0 @@
-export default {
-	token: state => state.app.token,
-	isLogin: state => !!state.app.token,
-	userInfo: state => state.app.userInfo || {},
-	uid: state => state.app.uid
-};
